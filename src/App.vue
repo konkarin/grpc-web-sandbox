052ec8a824c076db38264ae97b997c5f20a20555
konkarin <konkarin.photo@gmail.com>
2023-12-15 10:01:23 +0900

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 
+const res = ref('hello')
 </script>
 
 <template>
+  <h1>
+    grpc sandobox
+  </h1>
   <div>
-    
+    {{ res }}
   </div>
 </template>
