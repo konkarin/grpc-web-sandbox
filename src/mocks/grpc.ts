4f2d557f1276ce20c498abc12f661cb7281f6b87
konkarin <konkarin.photo@gmail.com>
2023-12-16 09:11:03 +0900

    docs: ✏️ add comments

--- src/mocks/grpc.ts
+++ src/mocks/grpc.ts
@@ -1,6 +1,28 @@
 /**
  * @see https://zenn.dev/link/comments/ee89092f84b746
+ * @see https://github.com/grpc/grpc-web/issues/634#issuecomment-1052189381
+ * @see https://lucas-levin.com/code/blog/mocking-grpc-web-requests-for-integration-testing
  */
+export const encodeProtobuf = (replyBuffer: Uint8Array) => {
+  const replyLength = toBytesInt32(replyBuffer.length);
+
+  const headers = Buffer.from([
+    ...createHeader("grpc-status", "0"),
+    ...createHeader("grpc-message", "OK"),
+  ]);
+  const headersLength = toBytesInt32(headers.length);
+
+  const response = Buffer.from([
+    0, // mark response start ("data" frame coming next)
+    ...replyLength,
+    ...replyBuffer,
+    128, // end of "data" frame, trailer frame coming next
+    ...headersLength,
+    ...headers,
+  ]);
+
+  return response;
+};
 
 const toBytesInt32 = (num: number): Uint8Array => {
   // an Int32 takes 4 bytes
@@ -23,25 +45,4 @@ const createHeader = (name: string, value: string): Buffer => {
   buffers.push("\r".charCodeAt(0));
   buffers.push("\n".charCodeAt(0));
   return Buffer.from(buffers);
-};
-
-export const encodeProtobuf = (replyBuffer: Uint8Array) => {
-  const replyLength = toBytesInt32(replyBuffer.length);
-
-  const headers = Buffer.from([
-    ...createHeader("grpc-status", "0"),
-    ...createHeader("grpc-message", "OK"),
-  ]);
-  const headersLength = toBytesInt32(headers.length);
-
-  const response = Buffer.from([
-    0, // mark response start ("data" frame coming next)
-    ...replyLength,
-    ...replyBuffer,
-    128, // end of "data" frame, trailer frame coming next
-    ...headersLength,
-    ...headers,
-  ]);
-
-  return response;
 };
